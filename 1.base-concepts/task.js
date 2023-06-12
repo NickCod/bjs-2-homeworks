36f7e2f9aa8a3c542fb21875c40892814db0afff
NickCode <Garfild.odi@mail.ru>
2023-06-12 16:04:33 +0300

    Первое задание переделано

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,6 +21,6 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   percent = (percent /100) /12;
   let body = amount - contribution;
   let payment = body * (percent +(percent / (((1+percent)**countMonths)-1)));
-  let summ = (payment * countMonths).toFixed(2);
+  let summ = +(payment * countMonths).toFixed(2);
   return summ;
 }
