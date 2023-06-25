422b189b5ee1333278b860d036d225df8cc4e44e
NickCode <Garfild.odi@mail.ru>
2023-06-25 19:44:41 +0300

    Пытаюсь сделать 3 дз

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,40 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    for ( let element of arr1 ){
+    }
+     for (let element of arr2){
+     }
+     return arr1.every((element, index) => element !== arr2[index]);
+   }
+   
+    let result = compareArrays([1,2,3,4,5,6], [1,2,3,4,5,6]);
+   
+   console.log(result) 
 
-function getUsersNamesInAgeRange(users, gender) {
+
+   const people = [
+    {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+    {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+    {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+    {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+    {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+    {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+    {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+    {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+    {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+    {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+    {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+    {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+    {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+    {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+  ];
+
+  function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(user => user.gender === gender);
+    const ages = filteredUsers.map(user => user.age);
+    const sumOfAges = ages.reduce((acc, age) => acc + age, 0);
+    const averageAge = sumOfAges / ages.length;
+    return averageAge;
+  }
   
-}
+  const averageAge = getUsersNamesInAgeRange(people, "мужской");
+  console.log(averageAge);
