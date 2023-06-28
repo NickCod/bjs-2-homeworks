696b86fc9025fa78f8b7cc1fbd5129965e4ba6b6
NickCode <Garfild.odi@mail.ru>
2023-06-28 18:59:10 +0300

    ВЫполнено 4 задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,40 +1,34 @@
-const student1 = new Student('Юлия', 'женский', 20);
-const student2 = new Student('Никита', 'мужской', 21);
-
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
+  this.name = name,
+  this.gender = gender,
+  this.age = age,
+  this.marks = []
 }
+let student = new Student ("Иван", "Мужской", 19);
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+  this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (this.hasOwnProperty('marks') === true)  {
-    this.marks.push(...marks);
-  } else {
-    this.marks = [...marks];
-  }
+  if (this.marks === undefined){
+    return "Студент отчислен"; 
+    } else {
+    this.marks.push(...marks); 
+    }
 }
 
 Student.prototype.getAverage = function () {
- if (this.marks.filter(mark => mark !== undefined).length > 0 ){
-    const result = this.marks.reduce((acc,item) => acc + item, 0 );
-    const average = result/ this.marks.length;
-    return average;
- } else {
-    return 0;
- }
+  if (this.marks === undefined || this.marks.length === 0) {
+     return 0;
+  } else {
+     return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length
+  }
 }
 
-Student.prototype.exclude = function(reason) {
-  if (!this.marks || Object.keys(this.marks).length === 0) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
-  }
-};
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
   
